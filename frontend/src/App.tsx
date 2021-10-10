8085e7eb0a6325c7fa60977a5536239a9b3e94fa
ben <benedikt.maciejczyk@gmx.de>
2021-10-10 16:55:17 +0200

    remove unneccessary redux files and import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import {Counter} from './features/counter/Counter';
 import './App.css';
 import StartView from "./Components/start-view/StartView";
 import Quiz from "./Components/quiz/Quiz";
