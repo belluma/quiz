ba2f73aec134a3b0f68e9726df3d8fefa75c285c
ben <benedikt.maciejczyk@gmx.de>
2021-10-10 12:40:51 +0200

    add code to service

--- src/main/java/com/example/quiz/service/QuizService.java
+++ src/main/java/com/example/quiz/service/QuizService.java
@@ -1,4 +1,46 @@
 package com.example.quiz.service;
 
+import com.example.quiz.model.Quizcard;
+import com.example.quiz.repository.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
 public class QuizService {
+
+    @Autowired
+    private final QuizRepository repository;
+
+    public QuizService(QuizRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public List<Quizcard> getAllCards() throws NoSuchElementException {
+        List <Quizcard> cards =repository.findAll();
+        if(cards.size() == 0) throw new NoSuchElementException("No quizcards created yet");
+        return cards;
+    }
+
+    public Quizcard createQuizcard(Quizcard quizcard) throws IllegalArgumentException {
+        handleCreateQuizcardErrors(quizcard);
+        return repository.save(quizcard);
+
+    }
+    private void handleCreateQuizcardErrors(Quizcard quizcard) throws IllegalArgumentException{
+        if(quizcard.getQuestion().length() == 0) throw new IllegalArgumentException("Question can't be empty");
+        if(quizcard.getChoices().size() == 0) throw new IllegalArgumentException("Choices can't be empty");
+        if(quizcard.getAnswerIndices().size() == 0) throw new IllegalArgumentException("Correct answer can't be empty");
+        if(quizcard.getChoices().size() <= Collections.max(quizcard.getAnswerIndices()))
+            throw new IllegalArgumentException("Index of correct answer out of bounds");
+        if(quizcard.getChoices().size() < quizcard.getAnswerIndices().size())
+            throw new IllegalArgumentException("More correct answers than choices given");
+        if(Collections.min(quizcard.getAnswerIndices()) < 0)
+            throw new IllegalArgumentException("Index of correct answer can't be negative");
+    }
+
 }
