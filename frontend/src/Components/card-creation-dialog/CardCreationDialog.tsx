2af1ea64de5017d63df8243e11314e3abf9b093c
ben <benedikt.maciejczyk@gmx.de>
2021-10-10 20:33:08 +0200

    save quizcard properties in state

--- frontend/src/Components/card-creation-dialog/CardCreationDialog.tsx
+++ frontend/src/Components/card-creation-dialog/CardCreationDialog.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Button, Card, CardContent} from "@mui/material";
+import TextField from '@mui/material/TextField';
 
 //component imports
 
@@ -6,9 +8,47 @@ import React from 'react'
 
 type Props = {};
 
-function CardCreationDialog(props: Props){
-    return(
-       <div>CardCreationDialog</div>
+function CardCreationDialog(props: Props) {
+    const [question, setQuestion] = useState<string>("");
+    const [choices, setChoices] = useState<string[]>([]);
+    const [choiceText, setChoiceText] = useState<string>("");
+    const [answerIndex, setAnswerIndex] = useState<number>(0);
+    const [answerIndices, setAnswerIndices] = useState<number[]>([]);
+    const handleChange = ({target}: React.ChangeEvent<HTMLInputElement>) => {
+        target.name === "question" ? setQuestion(target.value) :
+            target.name === "choiceText" ? setChoiceText(target.value) :
+                setAnswerIndex(+target.value);
+    }
+
+    const saveCoice = () => {
+        setChoices([...choices, choiceText]);
+        setChoiceText("");
+    }
+    const saveIndex = () => {
+        if(answerIndices.indexOf(answerIndex) >= 0) return;
+        setAnswerIndices([...answerIndices, answerIndex]);
+        setAnswerIndex(0);
+    }
+    return (
+        <Card>
+            <CardContent>
+                <TextField value={question} name="question" label="question" onChange={handleChange}></TextField>
+                <TextField value={choiceText} name="choiceText" label="choiceText" onChange={handleChange}></TextField>
+                <Button onClick={saveCoice}>add answer</Button>
+                <TextField
+                    value={answerIndex}
+                    label="answerIndex"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    inputProps={{min:0, max:choices.length -1}}
+                    onChange={handleChange}
+                    // min={0} max={choices.length - 1}
+                /><Button onClick={saveIndex}>add answer</Button>
+
+            </CardContent>
+        </Card>
     )
 }
 
