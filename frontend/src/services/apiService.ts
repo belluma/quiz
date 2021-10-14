ad967c391cdf77992bf7093c01fb2896028f9494
ben <benedikt.maciejczyk@gmx.de>
2021-10-14 07:06:31 +0200

    add function to apiService to address validateAnswer route

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -6,8 +6,8 @@ export const getAllCards = ():Promise<AxiosResponse> => {
     return axios.get(BASE_URL)
         .then(result => result)
         .catch(err => err)
-
 }
+
 export const createCard = (card:any):Promise<AxiosResponse> =>{
     return axios({
         method:'post',
@@ -16,6 +16,13 @@ export const createCard = (card:any):Promise<AxiosResponse> =>{
         headers:{"Content-Type": "application/json"}
     }).then(response => response)
         .catch(err => err)
-
-
+}
+export const validateCard = (card:any):Promise<AxiosResponse> =>{
+    return axios({
+        method:'post',
+        url:`${BASE_URL}`,
+        data: card,
+        headers:{"Content-Type": "application/json"}
+    }).then(response => response)
+        .catch(err => err)
 }
