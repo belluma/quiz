3a3fcb2df8970f167971e3766ebe7b62dc23fa99
ben <benedikt.maciejczyk@gmx.de>
2021-10-16 16:06:00 +0200

    create router that forwards routing to react if not on api/ route

---
+++ src/main/java/com/example/quiz/controller/ReactAppController.java
@@ -0,0 +1,18 @@
+package com.example.quiz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ReactAppController {
+
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
+    }
+
+}
+//https://stackoverflow.com/questions/47689971/how-to-work-with-react-routers-and-spring-boot-controller
