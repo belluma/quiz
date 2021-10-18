232a7493240d00f5bc992b8fe5b623755a808bcf
ben <benedikt.maciejczyk@gmx.de>
2021-10-18 15:49:49 +0200

    add routes to highscore controller

--- src/main/java/com/example/quiz/controller/HighscoreController.java
+++ src/main/java/com/example/quiz/controller/HighscoreController.java
@@ -1,4 +1,29 @@
 package com.example.quiz.controller;
 
+
+import com.example.quiz.model.DTO.HighscoreDTO;
+import com.example.quiz.service.HighscoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/highscore")
 public class HighscoreController {
+
+    @Autowired
+    private final HighscoreService service;
+
+    public HighscoreController(HighscoreService service) {
+        this.service = service;}
+
+    public List<HighscoreDTO>getHighscores(){
+        return service.getHighscores();
+    }
+    public HighscoreDTO saveHighscore(@RequestBody HighscoreDTO highscore){
+        return service.saveHighscore(highscore);
+    }
 }
