7d86574f2bbd2dc28cbcebb0c645e90fba3656ce
ben <benedikt.maciejczyk@gmx.de>
2021-10-19 14:06:22 +0200

    write first tests for cardfooter

--- frontend/src/Components/quizcard/card-footer/CardFooter.test.tsx
+++ frontend/src/Components/quizcard/card-footer/CardFooter.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CardFooter from './CardFooter';
+import {fireEvent, render, screen} from "@testing-library/react";
 
 let container: HTMLElement | null = null;
 beforeEach(() => {
@@ -8,14 +9,38 @@ beforeEach(() => {
     document.body.appendChild(container);
 });
 
-afterEach(() =>{
-    if(container){
-    ReactDOM.unmountComponentAtNode(container);
-    container.remove();
-    }    container = null;
+afterEach(() => {
+    if (container) {
+        ReactDOM.unmountComponentAtNode(container);
+        container.remove();
+    }
+    container = null;
 })
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-   // ReactDOM.render(<CardFooter />, div);
-    });
+    ReactDOM.render(<CardFooter disableButton={false} onButtonClick={() => {
+    }} buttonText={"button"} footerText={"footer"}/>, div);
+});
+
+describe("text props get forwarded", () => {
+    render(<CardFooter disableButton={false} onButtonClick={() => {
+    }} buttonText={"button"} footerText={"footer"}/>)
+    test("to button", () => {
+        expect(screen.getByText("button")).toBeInTheDocument();
+    })
+    // test("to typography component", () => {
+    //     expect(screen.getByRole('button', {name: "footer"})).toBeInTheDocument();
+    // // console.log(screen.getByText("footer"))
+    // })
+})
+
+// test("onButtonClick function gets executed on buttton click", () => {
+//     const onButtonClick = jest.fn();
+//     render(<CardFooter disableButton={false} onButtonClick={onButtonClick} buttonText={"button"}
+//                        footerText={"footer"}/>)
+//     const buttons = screen.getAllByRole("button")
+//     console.log(buttons)
+//     buttons.forEach((button) => fireEvent.click(button))
+//     // expect(onButtonClick).toHaveBeenCalledTimes(2);
+// })
