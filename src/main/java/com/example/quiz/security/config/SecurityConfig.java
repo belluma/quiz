01bc2213a72a31fc428402d669905fcbb4f4d3fa
ben <benedikt.maciejczyk@gmx.de>
2021-10-19 17:22:06 +0200

    disable csrf in security config

--- src/main/java/com/example/quiz/security/config/SecurityConfig.java
+++ src/main/java/com/example/quiz/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.quiz.security.service.UserAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,7 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    //use nopassencoder to make it easier !!1! important to change for production !!1!
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -33,12 +33,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 //to enable posts (and also delete/put???)
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/**").permitAll()
-//                .and().formLogin()
-//                .and().httpBasic();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().httpBasic();
+    }
 }
